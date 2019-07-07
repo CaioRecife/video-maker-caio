db773a29318c783752dfeffab5696a1d3874152f
Caio César <br.caiorecife@cesarleitao.com.br>
2019-07-07 17:18:29 -0300

    test

---
+++ robots/text.js
@@ -0,0 +1,42 @@
+const algorithmia = require('algorithmia')
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+
+async function robot(content){
+    await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    //breakContentIntoSentences(content)
+
+
+    async function fetchContentFromWikipedia(content){
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponde.get()
+        
+        content.sourceContentOriginal = wikipediaContent.content
+
+    }
+
+    function sanitizeContent(content){
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        console.log(withoutBlankLinesAndMarkdown)
+
+        function removeBlankLinesAndMarkdown(text){
+            const allLines = text.split('\n')
+            
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if(line.trim().length === 0 || line.trim().startsWith('=')){
+                    return false
+                }else{
+                return true
+                }
+            })
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+        
+    }
+   
+    
+}
+
+module.exports = robot
